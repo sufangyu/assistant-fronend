2cef59f96dcd0ac3b5b3f4660f17c3c3a03c5da2
方雨_Yu <304683191@qq.com>
2023-03-31 10:50:51 +0800

    chore: 调整 打包资源路径，解决代理后资源加载问题

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue(), vueJsx(), eslint()],
   resolve: {
     alias: {
