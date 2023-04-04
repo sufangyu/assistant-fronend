58fe630510836b468984d9d7014fd121c280cc50
sufangyu <304683191@qq.com>
2023-04-04 10:27:30 +0800

    feat: 兼容 低版本浏览器

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import eslint from 'vite-plugin-eslint'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue(), vueJsx(), eslint()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    eslint(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
